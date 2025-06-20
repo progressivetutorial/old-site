d006da5d952ae5713950c2a9f347683d842b1c0f
progressivetutorial <progressivetutorialbbsr@gmail.com>
2025-06-20 23:12:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-const hamburger = document.getElementById('hamburger');
-const navLinks = document.getElementById('navLinks');
-
-hamburger.addEventListener('click', () => {
-  navLinks.classList.toggle('show');
-});
+function toggleMenu() {
+  const nav = document.getElementById('navLinks');
+  nav.classList.toggle('show');
+}
